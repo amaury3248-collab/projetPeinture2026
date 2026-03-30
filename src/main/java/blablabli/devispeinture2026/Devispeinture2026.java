842c38d288186940fcd23b575dea4d7756a240d3
aringuet01 <aringuet01@SEC-C212-F06.csip.in>
2026-03-30 09:17:54 +0200

    premier commit du projet(feu)

---
+++ src/main/java/blablabli/devispeinture2026/Devispeinture2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package blablabli.devispeinture2026;
+
+/**
+ *
+ * @author aringuet01
+ */
+public class Devispeinture2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
