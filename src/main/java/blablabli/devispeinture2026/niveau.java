dbce65c618ea3c104639b57b4c0943bb2fa484ac
sauvegrainadrien-del <sauvegrain.adrien@gmail.com>
2026-04-10 12:12:31 +0200

    Update niveau.java

--- src/main/java/blablabli/devispeinture2026/niveau.java
+++ src/main/java/blablabli/devispeinture2026/niveau.java
@@ -1,1 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Niveau {
+    private String idNiveau;
+    private double h;
+    private int nbrAppart;
+
+    public Niveau(String idNiveau, double h) {
+        this.idNiveau = idNiveau;
+        this.h = h;
+        this.nbrAppart = nbrApart;
+    }
+
+    public void ajouterAppartement(Appartement appartement) {
+        this.appartements.add(appartement);
+    }
+}
