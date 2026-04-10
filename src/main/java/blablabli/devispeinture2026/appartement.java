a7419d0433f04c12125a00d08dc732e21ab4af2c
amaury3248-collab <amaury3248@gmail.com>
2026-04-10 12:09:19 +0200

    appartement.java

--- src/main/java/blablabli/devispeinture2026/appartement.java
+++ src/main/java/blablabli/devispeinture2026/appartement.java
@@ -1,1 +1,20 @@
-    System.out.println('blablabala');
+public class appartement {
+    private int idAppart;
+    private int nbrPiece;
+
+    public int getIdAppart() {
+        return idAppart;
+    }
+    public void setIdAppart(int idAppart) {
+        this.idAppart = idAppart;
+    }
+    public int getNbrPiece() {
+        return nbrPiece;
+    }
+    public void setNbrPiece(int nbrPiece) {
+        this.nbrPiece = nbrPiece;
+    }
+    
+    
+    
+}
