45f31c97904d4b9f2bc7868d9ac64e66b7573adb
sauvegrainadrien-del <sauvegrain.adrien@gmail.com>
2026-04-10 12:53:43 +0200

    Update niveau.java

--- src/main/java/blablabli/devispeinture2026/niveau.java
+++ src/main/java/blablabli/devispeinture2026/niveau.java
@@ -1,23 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
+package com.mycompany.projetpeinture2026;
 
-public class Niveau {
+import java.util.*;
+
+public class niveau {
     private String idNiveau;
     private double h;
     private int nbrAppart;
 
-    public Niveau(String idNiveau, double h, int nbrAppart) {
+    public niveau(String idNiveau, double h, int nbrAppart) {
         this.idNiveau = idNiveau;
         this.h = h;
-        this.nbrAppart = nbrApart;
+        this.nbrAppart = nbrAppart;
     }
-}
+
+    public String getIdNiveau() {
+        return idNiveau;
+    }
+
+    public void setIdNiveau(String idNiveau) {
+        this.idNiveau = idNiveau;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
 
     public int getNbrAppart() {
-        return this.nbrAppart;
+        return nbrAppart;
     }
 
     public void setNbrAppart(int nbrAppart) {
         this.nbrAppart = nbrAppart;
     }
+
+
 }
