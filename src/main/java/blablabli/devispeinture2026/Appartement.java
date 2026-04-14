00841f23eec9ae5538a0673c52cddbbef2c6be86
sauvegrainadrien-del <sauvegrain.adrien@gmail.com>
2026-04-14 23:42:42 +0200

    Refactor Appartement class and add pieces list

--- src/main/java/blablabli/devispeinture2026/Appartement.java
+++ src/main/java/blablabli/devispeinture2026/Appartement.java
@@ -1,27 +1,33 @@
 package blablabli.devispeinture2026;
 
-public class appartement {
+import java.util.List;
+
+public class Appartement {
     private int idAppart;
     private int nbrPiece;
+    private List<Piece> pieces;
 
-public appartement(int idAppart, int nbrPiece){
-    this.idAppart = idAppart;
-    this.nbrPiece = nbrPiece;
-}
-    
+    public Appartement(int idAppart, int nbrPiece, List<Piece> pieces) {
+        this.idAppart = idAppart;
+        this.nbrPiece = nbrPiece;
+        this.pieces = pieces;
+    }
+
     public int getIdAppart() {
         return idAppart;
-    }
-    public void setIdAppart(int idAppart) {
-        this.idAppart = idAppart;
     }
     public int getNbrPiece() {
         return nbrPiece;
     }
+    public List<Piece> getPieces() {
+        return pieces;
+    }
+
     public void setNbrPiece(int nbrPiece) {
         this.nbrPiece = nbrPiece;
     }
-    
-    
-    
+
+    public void ajouterPiece(Piece p){
+        if(p != null) this.pieces.add(p);
+    }
 }
