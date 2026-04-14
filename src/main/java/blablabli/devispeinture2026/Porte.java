05e84a527a4764b13da01cd0276799c5db2bc118
sauvegrainadrien-del <sauvegrain.adrien@gmail.com>
2026-04-14 23:49:23 +0200

    Add Porte class extending Ouverture with dimensions

---
+++ src/main/java/blablabli/devispeinture2026/Porte.java
@@ -0,0 +1,17 @@
+package com.mycompany.projetpeinture2026;
+
+public class Porte extends Ouverture {
+
+    public Porte() {
+        super(0.90, 2.10); //Dimensions standards
+    }
+
+    public double getLargeur() {
+        return largeur;
+    }
+    public double getHauteur() {
+        return hauteur;
+    }
+    
+    
+}
