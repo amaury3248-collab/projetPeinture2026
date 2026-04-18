722d2375682dc32670fcb0a3ef0ca59cf59d450b
amaury3248-collab <amaury3248@gmail.com>
2026-04-18 18:13:28 +0200

    Create SurfaceInvalideException.java

---
+++ src/main/java/blablabli/devispeinture2026/model/exceptions/SurfaceInvalideException.java
@@ -0,0 +1,7 @@
+package blablabli.devispeinture2026.model.exceptions;
+
+public class SurfaceInvalideException extends Exception {
+    public SurfaceInvalideException(String message) {
+        super(message);
+    }
+}
