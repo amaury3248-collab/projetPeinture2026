2409284be5aeadecebb1f11fd8c82952cc0a3eaf
amaury3248-collab <amaury3248@gmail.com>
2026-04-18 18:12:01 +0200

    Create HauteurIncoherenteException.java

---
+++ src/main/java/blablabli/devispeinture2026/model/exceptions/HauteurIncoherenteException.java
@@ -0,0 +1,7 @@
+package blablabli.devispeinture2026.model.exceptions;
+
+public class HauteurIncoherenteException extends Exception {
+    public HauteurIncoherenteException(String message) {
+        super(message);
+    }
+}
