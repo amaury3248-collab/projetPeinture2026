943bce950b4a56f246bb6cdcc8c3edaea609ff11
amaury3248-collab <amaury3248@gmail.com>
2026-04-18 18:12:32 +0200

    Create PorteInterditeSurMurExterneException.java

---
+++ src/main/java/blablabli/devispeinture2026/model/exceptions/PorteInterditeSurMurExterneException.java
@@ -0,0 +1,7 @@
+package blablabli.devispeinture2026.model.exceptions;
+
+public class PorteInterditeSurMurExterneException extends Exception {
+    public PorteInterditeSurMurExterneException(String message) {
+        super(message);
+    }
+}
