7c6314df7437d90eb3e37a2dc02dc46f6d88a07e
amaury3248-collab <amaury3248@gmail.com>
2026-04-18 18:13:13 +0200

    Create OuvertureInvalideException.java

---
+++ src/main/java/blablabli/devispeinture2026/model/exceptions/OuvertureInvalideException.java
@@ -0,0 +1,7 @@
+package blablabli.devispeinture2026.model.exceptions;
+
+public class OuvertureInvalideException extends Exception {
+    public OuvertureInvalideException(String message) {
+        super(message);
+    }
+}
