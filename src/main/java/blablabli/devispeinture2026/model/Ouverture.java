91d3008f907abd215a6ea31a6436af33a6836fb6
amaury3248-collab <amaury3248@gmail.com>
2026-04-19 15:16:49 +0200

    Update Ouverture.java

--- src/main/java/blablabli/devispeinture2026/model/Ouverture.java
+++ src/main/java/blablabli/devispeinture2026/model/Ouverture.java
@@ -1,23 +1,36 @@
 package blablabli.devispeinture2026.model;
 
 public abstract class Ouverture {
-    protected double largeur;
-    protected double hauteur;
 
-    public Ouverture(double largeur, double hauteur) {
+    protected double largeur;   // largeur de l’ouverture
+    protected double hauteur;   // hauteur de l’ouverture
+    protected double hBas;      // hauteur du bas de l’ouverture par rapport au sol
+
+    public Ouverture(double largeur, double hauteur, double hBas) {
         this.largeur = largeur;
         this.hauteur = hauteur;
+        this.hBas = hBas;
+    }
+
+    public double calculerSurface() {
+        return largeur * hauteur;
     }
 
     public double getLargeur() {
         return largeur;
     }
+
     public double getHauteur() {
         return hauteur;
     }
 
-    public double calculerSurface(){
-        return this.largeur*this.hauteur;
+    public double getHauteurBas() {
+        return hBas;
     }
-    
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() +
+               " (" + largeur + "m x " + hauteur + "m, hBas=" + hBas + "m)";
+    }
 }
