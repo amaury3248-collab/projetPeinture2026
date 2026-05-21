a162df2b6f8df7beb24970122b99752b1b70c091
amaury3248-collab <amaury3248@gmail.com>
2026-05-21 10:31:49 +0200

    Delete src/main/java/blablabli/devispeinture2026/model/exceptions/HauteurIncoherenteException.java

--- src/main/java/blablabli/devispeinture2026/model/exceptions/HauteurIncoherenteException.java
+++
@@ -1,7 +0,0 @@
-package blablabli.devispeinture2026.model.exceptions;
-
-public class HauteurIncoherenteException extends Exception {
-    public HauteurIncoherenteException(String message) {
-        super(message);
-    }
-}
