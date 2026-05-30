812e0f7eb2e5be765f35910febc48a5d624f32eb
sauvegrainadrien-del <sauvegrain.adrien@gmail.com>
2026-05-30 20:45:17 +0200

    Implement building loading from file
    Added method to load a building from a text file.

--- src/main/java/blablabli/devispeinture2026/model/Batiment.java
+++ src/main/java/blablabli/devispeinture2026/model/Batiment.java
@@ -1,5 +1,6 @@
 package com.mycompany.projetpeinture2026;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Batiment {
@@ -82,38 +83,132 @@ public class Batiment {
         System.out.println("Erreur de sauvegarde : " + e.getMessage());
     }
 }
-}
     
-package blablabli.devispeinture2026;
+        // Méthode pour charger un bâtiment à partir d'un fichier de sauvegarde texte
+    public static Batiment chargerBatiment(String nomFichier, List<Revetement> catalogue) {
+        Batiment batimentCharge = null;
+        Niveau niveauCourant = null;
+        Appartement appartCourant = null;
+        Piece pieceCourante = null;
+        Mur murCourant = null; // on garde la memoire sur le dernier mur lu 
 
-import java.util.List;
+        try (java.io.BufferedReader br = new java.io.BufferedReader(new java.io.FileReader(nomFichier))) {
+            String ligne;
+            while ((ligne = br.readLine()) != null) {
+                String[] d = ligne.split(";");
+                if (d.length == 0) continue;
 
-public class Batiment {
-    private int idBatiment;
-    private boolean type;
-    private List<Niveau> niveaux;
+                switch (d[0]) {
+                    case "BATIMENT":
+                        int idBat = Integer.parseInt(d[1]);
+                        boolean isImmeuble = d[2].equalsIgnoreCase("Immeuble");
+                        batimentCharge = new Batiment(idBat, isImmeuble, new ArrayList<>());
+                        break;
 
-    public Batiment(int idBatiment, boolean type, List<Niveau> niveaux) {
-        this.idBatiment = idBatiment;
-        this.type = type;
-        this.niveaux = niveaux;
+                    case "NIVEAU":
+                        if (batimentCharge != null) {
+                            int idNiv = Integer.parseInt(d[1]);
+                            double hauteur = Double.parseDouble(d[2]);
+                            niveauCourant = new Niveau(idNiv, hauteur, 0, new ArrayList<>());
+                            batimentCharge.ajouterNiveau(niveauCourant);
+                        }
+                        break;
+
+                    case "APPART":
+                        if (niveauCourant != null) {
+                            int idAppart = Integer.parseInt(d[1]);
+                            appartCourant = new Appartement(idAppart, 0, new ArrayList<>());
+                            niveauCourant.ajouterAppartement(appartCourant);
+                        }
+                        break;
+
+                    case "PIECE":
+                        if (appartCourant != null) {
+                            int idPiece = Integer.parseInt(d[1]);
+                            String usage = d[2];
+                            // Initialisation de strates vides (Sol et Plafond) pour la pièce
+                            Strate sol = new Strate(1, 0, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+                            Strate plafond = new Strate(2, 0, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+                            
+                            pieceCourante = new Piece(idPiece, usage, new ArrayList<>(), plafond, sol);
+                            appartCourant.ajouterPiece(pieceCourante);
+                        }
+                        break;
+                        
+                    case "REV_SOL":
+                        if (pieceCourante != null && pieceCourante.getStrateDown() != null) {
+                            int idRev = Integer.parseInt(d[1]);
+                            Revetement rev = trouverRevetementParId(idRev, catalogue);
+                            if (rev != null) pieceCourante.getStrateDown().ajouterRevSup(rev);
+                        }
+                        break;
+                        
+                    case "REV_PLAFOND":
+                        if (pieceCourante != null && pieceCourante.getStrateUp() != null) {
+                            int idRev = Integer.parseInt(d[1]);
+                            Revetement rev = trouverRevetementParId(idRev, catalogue);
+                            if (rev != null) pieceCourante.getStrateUp().ajouterRevInf(rev);
+                        }
+                        break;
+
+                    case "MUR":
+                        if (pieceCourante != null) {
+                            int idMur = Integer.parseInt(d[1]);
+                            double[] coords = new double[]{
+                                Double.parseDouble(d[2]), Double.parseDouble(d[3]),
+                                Double.parseDouble(d[4]), Double.parseDouble(d[5])
+                            };
+                            boolean isExt = d[6].equals("1");
+                            
+                            murCourant = new Mur(idMur, coords, isExt, new ArrayList<>(), new ArrayList<>());
+                            pieceCourante.ajouterMur(murCourant);
+                        }
+                        break;
+                    
+                    case "REV_MUR":
+                        if (murCourant != null) {
+                            int idRev = Integer.parseInt(d[1]);
+                            Revetement rev = trouverRevetementParId(idRev, catalogue);
+                            if (rev != null) murCourant.ajouterRevetement(rev);
+                        }
+                        break;
+                }
+            }
+            
+            // Recalcul mathématique des surfaces après chargement
+            if (batimentCharge != null) {
+                for (Niveau n : batimentCharge.getNiveaux()) {
+                    for (Appartement a : n.getApparts()) {
+                        for (Piece p : a.getPieces()) {
+                            // Si la pièce a bien ses murs (au moins la longueur et la largeur)
+                            if (p.getMurs().size() >= 2) {
+                                double longueur = p.getMurs().get(0).calculerLongueur();
+                                double largeur = p.getMurs().get(1).calculerLongueur();
+                                double vraieSurface = longueur * largeur;
+                                
+                                // On met à jour les surfaces à 0 avec les vraies valeurs
+                                if (p.getStrateDown() != null) p.getStrateDown().setSurfaceBrute(vraieSurface);
+                                if (p.getStrateUp() != null) p.getStrateUp().setSurfaceBrute(vraieSurface);
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println("Chargement réussi depuis " + nomFichier);
+        } catch (Exception e) {
+            System.out.println("Erreur lors du chargement du fichier : " + e.getMessage());
+        }
+        return batimentCharge;
     }
-
-    public int getIdBatiment() {
-        return idBatiment;
-    }
-    public boolean isType() {
-        return type;
-    }
-    public List<Niveau> getNiveaux() {
-        return niveaux;
-    }
-
-    public void setType(boolean type) {
-        this.type = type;
-    }
-
-    public void ajouterNiveau(Niveau n) {
-        if (n != null) this.niveaux.add(n);
+    
+        // Méthode pour retrouver un revêtement par son ID
+    private static Revetement trouverRevetementParId(int id, List<Revetement> catalogue) {
+        for (Revetement r : catalogue) {
+            if (r.getIdRev() == id) {
+                return r;
+            }
+        }
+        return null;
     }
 }
