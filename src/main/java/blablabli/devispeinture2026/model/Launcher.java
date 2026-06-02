b95e9d6767d6ed93937c296476d2fd37fe77069f
sauvegrainadrien-del <sauvegrain.adrien@gmail.com>
2026-06-02 18:46:45 +0200

    Update Launcher.java

--- src/main/java/blablabli/devispeinture2026/model/Launcher.java
+++ src/main/java/blablabli/devispeinture2026/model/Launcher.java
@@ -6,7 +6,3 @@ public class Launcher {
         MainApp.main(args);
     } 
 }
-
-// Il y avait un problème une perturbation du chargeur de la machine virutelle Java. La JVM n'a pas réussi à isoler la méthode main correctement.
-//On ne peut pas lancer une classe qui hérite de javafx.application.Application via le plugin Maven Exec.
-// On crée cette classe afin qu'elle n'hérite pas de JavaFX
